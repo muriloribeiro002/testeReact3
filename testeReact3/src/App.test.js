6711431baf21db269da6960940547044f87c08a6
Murilo Ribeiro <106210940+muriloribeiro002@users.noreply.github.com>
2025-09-01 11:28:00 -0300

    Update App.test.js

--- testeReact3/src/App.test.js
+++ testeReact3/src/App.test.js
@@ -1,8 +1,8 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("renderiza o título da lista de tarefas", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const titleElement = screen.getByText(/lista de tarefas/i);
+  expect(titleElement).toBeInTheDocument();
 });
